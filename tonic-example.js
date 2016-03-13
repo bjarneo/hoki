dd372c2e82929c1343d72cecd5c2d5ae1842facf
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2016-03-13 09:46:19 +0100

    add tonic example

---
+++ tonic-example.js
@@ -0,0 +1,14 @@
+const register = require('hoki').register;
+const observer = require('hoki').observer;
+const dispatch = require('hoki').dispatch;
+
+// Register your event
+register('cat-names');
+
+// Observe for an event
+observer('cat-names', console.log);
+
+// Dispatch events with data
+dispatch('cat-names', 'furguson');
+dispatch('cat-names', 'mittens');
+dispatch('cat-names', 'boots');
